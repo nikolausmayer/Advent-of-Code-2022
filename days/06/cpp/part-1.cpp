0d84fc93b6a766bf36b81a1b0ce78e688ed06713
Nikolaus Mayer <mayern@cs.uni-freiburg.de>
2022-12-07 22:18:34 +0100

    day 06 c++

---
+++ days/06/cpp/part-1.cpp
@@ -0,0 +1,39 @@
+
+#include <cstdint>
+#include <algorithm>
+#include <fstream>
+#include <map>
+#include <regex>
+#include <set>
+#include <vector>
+#include <fmt/core.h>
+
+int main(int argc, char* argv[]) 
+{
+  if (argc < 2)
+    return EXIT_FAILURE;
+  std::ifstream infile{argv[1]};
+  if (infile.bad() or not infile.is_open())
+    return EXIT_FAILURE;
+
+  /// The real input is just a single line, but this makes the tests easier
+  for (std::string line;;) {
+    std::getline(infile, line);
+    if (infile.eof())
+      break;
+    
+    for (std::size_t end = 4; end <= line.size(); ++end) {
+      std::set<char> chars;
+      for (auto i = end - 4; i < end; ++i) {
+        chars.insert(line[i]);
+      }
+      if (chars.size() == 4) {
+        fmt::print("Marker {} at {}\n", line.substr(end - 4, 4), end);
+        break;
+      }
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
+
